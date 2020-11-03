c77de48c0eb28e0f6645eb50a309ee6077b7ac02
Nicholas Silas <njsilas@gmail.com>
2020-11-02 23:33:11 -0500

    commit tree 798e9d89e5dc7d678a41b626b9a5c360f6a5850f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+class School
+    attr_accessor :school, :student, :grade, :roster
+    
+    def initialize(school)
+        @school = school
+        @roster = {}
+       
+    end
+
+        def add_student(student, grade)
+           
+            
+             
+
+          if @roster[grade]
+             @roster[grade] << student 
+          else
+            @roster[grade] = []
+            @roster[grade] << student 
+          end
+        end
+        def grade(grade)
+            @roster.fetch(grade)
+
+        end
+    def sort
+        roster.each_value { |v| v.sort! }
+        
+    
+            
+        
+        
+        
+    end
+end
+
+
+
+
+
